c1dd0819e2d100f7690e6fc4631006f41dbf0038
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2022-04-27 10:33:23 -0500

    User form function added

--- team2/src/project.js
+++ team2/src/project.js
@@ -1,34 +1,58 @@
 const mobileMenu = document.getElementById('mobile-menu')
 const navMenu = document.querySelector('.nav-list')
 
-
 // Mobil menu
 mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('active')
     navMenu.classList.toggle('active')
     console.log("hi")
 })
-// User form
 
-userData = {
-    userId: 1,
-    events: {
-        name: "",
-        URL: "",
-        date: "",
+/* User form
+    - Validate form data before storing
+    - Store form data in userData.ideas[] 
+    - Clear form data after submit
+*/
+function addIdea(event) {
+    event.preventDefault() // Stops form reload on submit
+
+    // Get radio input
+    let checkedRadio = ''
+    let radioElements = document.getElementsByName('category')
+    console.log(radioElements)
+
+    for(let i =0 ; i< radioElements.length; i++) {
+        if(radioElements[i].checked) {
+            // incomplete
+        }
+    }
+
+    let idea = {
+        id: Date.now(),
+        name: document.getElementById('event-name').value,
+        URL: document.getElementById('event-url').value,
+        date: document.getElementById('event-date').value,
         category: ""
     }
+    userData.ideas.push(idea)
+
+    console.log("Added idea",userData)
+
+    document.querySelector('form').reset()
+
+    // Add updated userData to localstorage
+    localStorage.setItem('myIdeaList', JSON.stringify( userData))
 }
 
-const formName = document.getElementById('event-name')
-const formURL = document.getElementById('event-url')
-const formDate = document.getElementById('event-date')
-const formCategory = document.getElementById('')
-
-
-function setData(event) {
-    // Stops page reload
-    event.preventDefault()
-
-    console.log(userData)
-}
+// Runs once when page is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // TODO: Import user data if previously submitted
+    userData = {
+        userId: 1,
+        userName: 'Default',
+        ideas: []
+    }
+    
+    // Call function addIdea on form-btn click
+    document.querySelector('.form-btn').addEventListener('click', addIdea)
+})
