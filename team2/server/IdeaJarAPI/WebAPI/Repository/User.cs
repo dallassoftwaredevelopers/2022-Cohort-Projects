11db8917de94792cde96588e04ab64f18ffd6ec8
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-05-05 00:12:55 -0400

    Add Email property to User Repo

--- team2/server/IdeaJarAPI/WebAPI/Repository/User.cs
+++ team2/server/IdeaJarAPI/WebAPI/Repository/User.cs
@@ -3,6 +3,7 @@
     public class User
     {
         public string Username { get; set; }
+        public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
     }
