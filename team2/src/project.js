dc15110d6905f2077f067fd32db1dcf2f9a9bd48
robertsonj48 <robertsonj48@gmail.com>
2022-05-04 20:57:19 -0400

    added functionality to get idea page

--- team2/src/project.js
+++ team2/src/project.js
@@ -1,20 +1,70 @@
 // Isolated for get-idea.html
 function startGetIdea() {
-    $(document).click(function() {
-        $( "#jar" ).effect( "shake", {direction: "up", times: 4, distance: 10}, 1000 );
-      });
-  
-    /* modal on get idea */
-    $(".open").on("click", function() {
-        setTimeout(() => {
-          $(".popup-overlay, .popup-content").addClass("active");
-      }, 1300)
-        });
 
-      $(".close, .popup-overlay").on("click", function() {
+
+    /* modal on get idea */
+    $(".open").on("click", function () {
+        let chosenCategory = this.classList[0]
+
+        $("#jar").effect("shake", {
+            direction: "up",
+            times: 4,
+            distance: 10
+        }, 1000);
+
+        getIdea(chosenCategory)
+
+        setTimeout(() => {
+            $(".popup-overlay, .popup-content").addClass("active");
+        }, 1300)
+    });
+
+    $(".close, .popup-overlay").on("click", function () {
         $(".popup-overlay, .popup-content").removeClass("active");
-      });
+    });
+
+
+    userData = JSON.parse(localStorage.getItem('myIdeaList'));
+    popUpWindow = document.querySelector(".popup-content > h2");
+
+
+    function getIdea(chosenCategory) {
+        //reset optional data fields
+        document.querySelector(".popup-url").innerText = "";
+        document.querySelector(".popup-date").innerText = "";
+
+        //validates user choice and provides feedback
+        if (!userData) {
+            popUpWindow.innerText = "You have no activities in this category";
+            return
+        }
+        //filters ideas based on chosen category
+        let ideasInCategory = userData.ideas.filter(idea => idea.category == chosenCategory);
+
+        if (chosenCategory == "other") {
+            ideasInCategory = userData.ideas;
+        }
+
+        generateRandomIdea(ideasInCategory);
+        
+    }
 }
+//generates a random index and uses it to pick a random idea
+function generateRandomIdea(ideasInCategory){
+    let randomIdeaIndex = Math.floor(Math.random() * ideasInCategory.length);
+    let randomIdea = ideasInCategory[randomIdeaIndex];
+
+
+    popUpWindow.innerText = randomIdea.name;
+    if (randomIdea.URL) {
+        document.querySelector(".popup-url").innerText = randomIdea.URL;
+    }
+    if (randomIdea.date) {
+        document.querySelector(".popup-date").innerText = `Date: ${randomIdea.date}`;
+    }
+}
+
+
 
 /* Mobile menu
     - applies to all pages
@@ -26,7 +76,6 @@ const navMenu = document.querySelector('.nav-list')
 mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('active')
     navMenu.classList.toggle('active')
-    console.log("hi")
 })
 
 /* User form
@@ -36,17 +85,17 @@ mobileMenu.addEventListener('click', () => {
 
 let userData = []
 
-function displayIdeas() {    
+function displayIdeas() {
     let ideaArray = userData.ideas
 
     // Creates a table row for every existing entry
-    for (let i=0 ; i< ideaArray.length; i++) {
-        createRow( ideaArray[i].name, ideaArray[i].URL, ideaArray[i].date, ideaArray[i].category)
+    for (let i = 0; i < ideaArray.length; i++) {
+        createRow(ideaArray[i].name, ideaArray[i].URL, ideaArray[i].date, ideaArray[i].category)
     }
 }
 
-function createRow( name, url, date, category) {
-    console.log(name,url,date,category)
+function createRow(name, url, date, category) {
+    console.log(name, url, date, category)
     let ideaTable = document.querySelector('.idea-table')
 
     let row = document.createElement('tr')
@@ -58,23 +107,7 @@ function createRow( name, url, date, category) {
     cell1.innerHTML = name
     cell2.innerHTML = date
     cell3.innerHTML = `<a href="${url}">${url}</a>`
-    
-    // Matches category value to appropriate css class
-    let categoryLabel = ''
-    if (category === 'stay-home')
-        categoryLabel = 'label-home'
-    else if ( category === 'restaurant' )
-        categoryLabel = 'label-restaurant'
-    else if ( category === 'road-trip' )
-        categoryLabel = 'label-trip'
-    else if ( category === 'indoors' )
-        categoryLabel = 'label-indoor'
-    else if ( category === 'outdoors' )
-        categoryLabel = 'label-outdoor'
-    else 
-        categoryLabel = 'label-any'
-
-    cell4.innerHTML = `<label class="${categoryLabel} category-options">${category}</label>`
+    cell4.innerHTML = `<label class="${category} category-options">${category}</label>`
 
     ideaTable.appendChild(row)
 
@@ -87,8 +120,8 @@ function addIdea(event) {
     let checkedRadio = ''
     let radioElements = document.getElementsByName('category')
 
-    for(let i =0 ; i< radioElements.length; i++) {
-        if(radioElements[i].checked) {
+    for (let i = 0; i < radioElements.length; i++) {
+        if (radioElements[i].checked) {
             // incomplete
             console.log(radioElements[i].value)
             checkedRadio = radioElements[i].value
@@ -96,7 +129,7 @@ function addIdea(event) {
     }
 
     // Validate input 
-    if( !document.getElementById('event-name').value || checkedRadio == "") {
+    if (!document.getElementById('event-name').value || checkedRadio == "") {
         alert("Please enter both an event name and category")
         console.log("first")
         return
@@ -114,7 +147,7 @@ function addIdea(event) {
     document.querySelector('form').reset()
 
     // Add updated userData to localstorage
-    localStorage.setItem('myIdeaList', JSON.stringify( userData))
+    localStorage.setItem('myIdeaList', JSON.stringify(userData))
 
     // Update idea table
     createRow(idea.name, idea.URL, idea.date, idea.category)
@@ -130,7 +163,7 @@ function startAddIdea() {
     // Import user data if previously submitted
     userData = JSON.parse(localStorage.getItem('myIdeaList'))
 
-    if(!userData) {
+    if (!userData) {
         userData = {
             userId: 1,
             userName: 'Default',
@@ -141,7 +174,7 @@ function startAddIdea() {
 
     // Populate data on page if present
     displayIdeas()
-    
+
     // Call function addIdea on form-btn click
     document.querySelector('.form-btn').addEventListener('click', addIdea)
 }
