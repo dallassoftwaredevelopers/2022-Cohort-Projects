7f9a5542def93040e094ef11a83180a96330790d
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-05-04 22:05:46 -0400

    Update AuthController

--- team2/server/IdeaJarAPI/WebAPI/Controllers/AuthController.cs
+++ team2/server/IdeaJarAPI/WebAPI/Controllers/AuthController.cs
@@ -39,26 +39,6 @@ namespace WebAPI.Controllers
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
 
-            var query = @$"INSERT INTO user (first_name, last_name, email, password_hash, password_salt) VALUES ('Ariel', 'Rodriguez', 'ariel@gmail.com', '{passwordHash}', '12345')";
-
-            var table = new DataTable();
-            var dataSource = _configuration.GetSection("Database:IdeaJarConnectionString").Value;
-            Console.WriteLine(dataSource);
-            MySqlDataReader reader;
-
-            using (var connection = new MySqlConnection(dataSource))
-            {
-                connection.Open();
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    reader = command.ExecuteReader();
-                    table.Load(reader);
-
-                    reader.Close();
-                }
-            }
-
-
             return Ok(user);
         }
 
@@ -76,6 +56,7 @@ namespace WebAPI.Controllers
         {
             if (user.Username != request.Username || !VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
                 return BadRequest("Wrong username/password combination.");
+
             // TODO:
             //if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
             //    return BadRequest("Wrong password.");
