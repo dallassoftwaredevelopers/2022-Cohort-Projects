be348c7a10cbfd71074e97a7a9f541a602d7e22a
Juan G. Vargas Jr <jgvargasjr@outlook.com>
2022-05-12 19:58:14 -0500

    Revert "Implement auth service"

--- team2/server/IdeaJarAPI/WebAPI/Data/DbInitializer.cs
+++ team2/server/IdeaJarAPI/WebAPI/Data/DbInitializer.cs
@@ -1,18 +1,19 @@
-﻿using System.Text;
-using WebAPI.Models;
+﻿using WebAPI.Models;
 
 namespace WebAPI.Data
 {
     public class DbInitializer
     {
-        public static void Initialize(ApplicationDbContext context)
+        public static void Initialize(IdeaJarContext context)
         {
             // Look for any users
             if (context.Users.Any()) return;
 
             var users = new User[]
             {
-                new User { Username = "bob", Email = "bobcom", PasswordHash = Encoding.ASCII.GetBytes("123"), PasswordSalt = Encoding.ASCII.GetBytes("456") },
+                //new User { Username = "bob", Email = "bob@email.com", PasswordHash = "123", PasswordSalt = "456" },
+                new User { Username = "sue", Email = "sue@email.com", PasswordHash = "1234", PasswordSalt = "4567" },
+                //new User { Username = "jane", Email = "jane@email.com", PasswordHash = "12345", PasswordSalt = "45678" },
             };
 
             context.Users.AddRange(users);
