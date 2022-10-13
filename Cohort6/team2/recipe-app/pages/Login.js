c2cf2fe589858e15c9253cb3aa8738e6d7b5762c
bycho91 <bycho1991@gmail.com>
2022-10-12 21:28:57 -0500

    adding the login page and login page styles

---
+++ Cohort6/team2/recipe-app/pages/Login.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Login.module.scss";
+
+const Login = () => {
+  return (
+    <div className={styles.main}>
+      <h1>This is the login page</h1>
+    </div>
+  );
+};
+
+export default Login;
