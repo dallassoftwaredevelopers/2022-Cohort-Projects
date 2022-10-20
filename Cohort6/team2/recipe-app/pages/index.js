50d399d45f782a0f84249da39527822932fce5e4
bycho91 <bycho1991@gmail.com>
2022-10-20 09:58:53 -0500

    testing

--- Cohort6/team2/recipe-app/pages/index.js
+++ Cohort6/team2/recipe-app/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Holiday Meals</title>
+        <title>GobbleMeal</title>
         <meta name="description" content="Search your favorite holiday meals" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
