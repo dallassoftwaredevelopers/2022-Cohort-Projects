cb06d0398abd22541fc4a1240a9018b97ee7010e
bycho91 <bycho1991@gmail.com>
2022-10-24 23:36:26 -0500

    consolidated navbar to one component

--- Cohort6/team2/recipe-app/pages/index.js
+++ Cohort6/team2/recipe-app/pages/index.js
@@ -1,19 +1,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 import Navbar from "@/components/Navbar";
-import DesktopNavbar from "@/components/DesktopNavbar";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>GobbleMeal</title>
+        <title>Gobble Meal</title>
         <meta name="description" content="Search your favorite holiday meals" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.home}>
         <Navbar />
-        <DesktopNavbar />
       </main>
     </div>
   );
