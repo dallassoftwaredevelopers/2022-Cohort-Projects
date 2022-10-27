d0ba6292e2d9e31e07bf34e14c8a535f589b3b0a
Sabina <109144680+Srasulova@users.noreply.github.com>
2022-10-26 23:47:14 -0400

    Update layout (WIP)

--- Cohort6/team-a-witchs-brew/src/routes/+layout.svelte
+++ Cohort6/team-a-witchs-brew/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import "../app.css";
 </script>
 
-<nav class="flex items-center text-center flex-wrap bg-purple-900 p-4">
+<nav class="flex items-center text-center flex-wrap p-4">
   <div class="text-white">
     <span class="font-semibold text-xl">Witch's Brew</span>
   </div>
@@ -19,51 +19,40 @@
 
   <!-- ----------------------------- -->
 
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+  <!--TODO: remove hover over color and put one in css-->
+  <div class="nav-options w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
+      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-200 mr-4">
         Home
       </a>
-      <a href="/recipes" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
+      <a href="/recipes" class="block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-200  mr-4">
         Recipes
       </a>
-      <a href="/featured" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
+      <a href="/featured" class="block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-200 mr-4">
         Featured
       </a>
-      <a href="/about" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
+      <a href="/about" class="block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-200 mr-4">
         About Us
       </a>
     </div>
 
     <!-- TODO SR: requires adjustments -->
-    <div>
-      <div class="flex justify-center">
-        <div class="mb-3 xl:w-96">
-          <input
-            type="search"
-            class="
-              form-control
-              block
-              w-full
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-            "
-            id="search"
-            placeholder="Search"
-          />
-        </div>
+ 
+<form class="flex items-center">   
+  <label for="simple-search" class="sr-only">Search</label>
+  <div class="relative w-full">
+      <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <svg aria-hidden="true" class="magnifying-glass w-5 h-5"  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
       </div>
-    </div>
+      <input 
+        type="text" 
+        id="simple-search" 
+        class="nav-search-bar text-sm block pl-10 p-2.5" 
+        placeholder="Search..." >
+  </div>
+ 
+</form>
+
 
   </div>
 </nav>
