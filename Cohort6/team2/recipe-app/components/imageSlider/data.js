38621bea175c1de045211d0e51cab1352b003df0
RabeDatta <robedutt@gmail.com>
2022-11-02 20:29:15 -0400

    added data required for slides

---
+++ Cohort6/team2/recipe-app/components/imageSlider/data.js
@@ -0,0 +1,24 @@
+import turkeyImg from "@/assets/images/turky.jpg";
+import groupLunch from "@/assets/images/groupLunch.jpg";
+import fall from "@/assets/images/fall.jpg";
+
+export const data = [
+  {
+    title: "Easy, No-Fuss Thanksgiving Turkey",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt odit illum illo. Quod, dolorem dolorum. Illum obcaecati quo, accusantium doloribus facere reprehenderit possimus. Qui magnam, dignissimos quasi cum quidem voluptatem.",
+    src: turkeyImg,
+  },
+  {
+    title: "Easy, No-Fuss Thanksgiving Turkey",
+    description:
+      "Voluptates nulla odit velit, excepturi dolorem deserunt! Molestiae adipisci est provident praesentium dicta",
+    src: groupLunch,
+  },
+  {
+    title: "Easy, No-Fuss Thanksgiving Turkey",
+    description:
+      "Ab, modi delectus velit et similique harum, itaque veniam iure, magnam voluptatibus eum aliquam praesentium enim aperiam quibusdam? Iure voluptate laborum eveniet",
+    src: fall,
+  },
+];
