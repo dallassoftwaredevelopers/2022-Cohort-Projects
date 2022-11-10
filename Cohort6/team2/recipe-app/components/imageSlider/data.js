dd3e88ec798ef3f3f1760822a73a8d7024a33fdb
RabeDatta <robedutt@gmail.com>
2022-11-09 21:49:21 -0500

    updated data

--- Cohort6/team2/recipe-app/components/imageSlider/data.js
+++ Cohort6/team2/recipe-app/components/imageSlider/data.js
@@ -1,24 +1,34 @@
 import turkeyImg from "@/assets/images/turky.jpg";
 import groupLunch from "@/assets/images/groupLunch.jpg";
 import fall from "@/assets/images/fall.jpg";
+import turkeyTwo from "@/assets/images/turkeyTwo.webp";
+import pie from "@/assets/images/pie.jpeg";
+import family from "@/assets/images/family.jpeg";
+import turkeyThree from "@/assets/images/turkeyThree.jpeg";
 
 export const data = [
   {
     title: "Easy, No-Fuss Thanksgiving Turkey",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt odit illum illo. Quod, dolorem dolorum. Illum obcaecati quo, accusantium doloribus facere reprehenderit possimus. Qui magnam, dignissimos quasi cum quidem voluptatem.",
-    src: turkeyImg,
+    src: turkeyThree,
   },
   {
     title: "Easy, No-Fuss Thanksgiving Turkey",
     description:
       "Voluptates nulla odit velit, excepturi dolorem deserunt! Molestiae adipisci est provident praesentium dicta",
-    src: groupLunch,
+    src: family,
   },
   {
     title: "Easy, No-Fuss Thanksgiving Turkey",
     description:
       "Ab, modi delectus velit et similique harum, itaque veniam iure, magnam voluptatibus eum aliquam praesentium enim aperiam quibusdam? Iure voluptate laborum eveniet",
-    src: fall,
+    src: pie,
+  },
+  {
+    title: "Easy, No-Fuss Thanksgiving Turkey",
+    description:
+      "Ab, modi delectus velit et similique harum, itaque veniam iure, magnam voluptatibus eum aliquam praesentium enim aperiam quibusdam? Iure voluptate laborum eveniet",
+    src: turkeyTwo,
   },
 ];
